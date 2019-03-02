ac2b62fa6020ab20af6630c24d6ca7c541ed2ba2
SRCinVA <scitarella@hotmail.com>
2019-03-01 20:40:24 -0500

    Add files via upload

---
+++ Stella_Mira_website_draft/234CompleteWebPage.js
@@ -0,0 +1,17 @@
+
+    var section = document.querySelectorAll('.section');
+    var sections = {};
+    section.forEach(function (e){
+        console.log(e.id, e.offsetTop) //offset shows how many px from the top.
+        sections[e.id] = e.offsetTop;
+    })
+    window.onscroll = function(){
+        var scrollPos = document.documentElement.scrollTop || document.body.scrollTop;
+        console.log(scrollPos);
+        for (var x in sections) {
+            if (sections[x] <= scrollPos) {
+                document.querySelector('.active').setAttribute('class', ' ');
+                document.querySelector('a[href*='+x+']').setAttribute('class','active')
+            }
+        }
+    }
